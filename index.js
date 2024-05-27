605e4bfd2bb607abf18bebcb4d5728bb2c6c9df5
Wildan <97178372+sonofhebe@users.noreply.github.com>
2024-05-27 17:07:05 +0700

    add chat and group and api

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+const express = require("express");
 const {
   makeWASocket,
   DisconnectReason,
   useMultiFileAuthState,
 } = require("@whiskeysockets/baileys");
 
+const app = express();
+let sock;
+
 async function connectToWhatsApp() {
   const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
-  const sock = makeWASocket({
-    // can provide additional config here
+  sock = makeWASocket({
     printQRInTerminal: true,
     auth: state,
   });
@@ -39,33 +42,83 @@ async function connectToWhatsApp() {
     if (message.key.fromMe) return; // Ignore messages sent by your own number
 
     const jid = message.key.remoteJid; // JID (Jabber ID) of the sender
-    const messageText = message.message.conversation; // Content of the message
+    let replyText = "";
 
-    // Custom logic to determine response based on message content
-    let replyText = `Hi,
-This is Wildan's temporary WhatsApp account. Wildan's main WhatsApp account (6289650973972) is now active again. This temporary account will be deleted soon, so please remove it from your contacts. This message will be forwarded to the main WhatsApp account. Thank you.`;
-
+    // replyText = `Hi,
+    // This is Wildan's temporary WhatsApp account. Wildan's main WhatsApp account (6289650973972) is now active again. This temporary account will be deleted soon, so please remove it from your contacts. This message will be forwarded to the main WhatsApp account. Thank you.`;
     // forward chat
-    const forwardJid = "6289650973972@s.whatsapp.net"; // JID of the recipient contact
-    const messageId = message.key.id; // ID of the message to forward
-    await sock.sendMessage(forwardJid, {
-      text: `Forward text
-from : ${jid.split("@")[0]}
+    // const forwardJid = "6289650973972@s.whatsapp.net"; // JID of the recipient contact
+    // const messageId = message.key.id; // ID of the message to forward
+    //     await sock.sendMessage(forwardJid, {
+    //       text: `Forward text
+    // from : ${jid.split("@")[0]}
 
-${messageText}`,
-      quoted: { key: { remoteJid: jid, id: messageId } },
-    });
+    // ${messageText}`,
+    //       quoted: { key: { remoteJid: jid, id: messageId } },
+    //     });
 
-    // if (messageText.toLowerCase().includes("hello")) {
-    //   replyText = "Hi there! How can I help you?";
-    // } else if (messageText.toLowerCase().includes("how are you")) {
-    //   replyText = "I'm doing well, thank you! How about you?";
-    // }
-
-    // Send the reply
-    await sock.sendMessage(jid, { text: replyText });
+    // =================================== AUTO REPLY =======================================
+    if (jid.endsWith("@g.us")) {
+      const messageText = message.message.conversation; // Content of the message
+      // ------------- GROUP -----------
+      return;
+    } else {
+      const messageText = message.message.extendedTextMessage.text; // Content of the message
+      // ------------- PC -----------
+      if (messageText.toLowerCase().includes("hello")) {
+        replyText = `Hi ${message.pushName}! How can I help you?`;
+        await sock.sendMessage(jid, { text: replyText });
+      } else if (messageText.toLowerCase().includes("how are you")) {
+        replyText = "I'm doing well, thank you! How about you?";
+        await sock.sendMessage(jid, { text: replyText });
+      }
+    }
+    // ================================= END AUTO REPLY =======================================
   });
 }
 
-// Run the function to connect to WhatsApp
-connectToWhatsApp();
+// =================================== API =======================================
+app.post("/send-message", async (req, res) => {
+  try {
+    const { phone, group, message } = req.query;
+    if (!phone && !group) {
+      return res.status(400).send("ID recivier are required");
+    }
+    if (!message) {
+      return res.status(400).send("Message are required");
+    }
+    let jid;
+    if (group) {
+      jid = group + "@g.us";
+    } else if (phone) {
+      jid = phone + "@s.whatsapp.net";
+    }
+
+    await sock.sendMessage(jid, { text: message });
+    res.status(200).send("Message sent successfully");
+  } catch (error) {
+    console.error("Failed to send message:", error);
+    res.status(500).send("Failed to send message");
+  }
+});
+app.post("/group-list", async (req, res) => {
+  try {
+    if (!sock) {
+      return res.status(500).send("WhatsApp socket is not initialized");
+    }
+
+    const groups = await sock.groupFetchAllParticipating();
+    res.status(200).json(groups);
+  } catch (error) {
+    console.error("Failed to get groups:", error);
+    res.status(500).send("Failed to get groups");
+  }
+});
+// =================================== API END =======================================
+
+// Start the Express server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectToWhatsApp();
+});
